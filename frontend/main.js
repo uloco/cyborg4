af32c7892d53d741987ec8398142896882886d22
Umut Topuzoğlu <uloco@schiefewelt.de>
2018-10-19 22:00:53 +0200

    Add mqtt lib

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,10 @@
 // video stream
-let video = document.querySelector("#video");
 if (Hls.isSupported()) {
   let video = document.querySelector("#video");
   let hls = new Hls();
   // bind them together
   hls.attachMedia(video);
   hls.on(Hls.Events.MEDIA_ATTACHED, () => {
-    console.log("video and hls.js are now bound together !");
     hls.loadSource(
       "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8"
     );
@@ -16,7 +14,8 @@ if (Hls.isSupported()) {
       );
     });
   });
-  video.play();
+  // TODO: uncomment to play
+  // video.play();
 } else {
   console.error("HLS Video format is not supported");
 }
